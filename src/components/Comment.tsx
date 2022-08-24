bc49650f7d4153e94a028c24853b2fab3dfe9962
Luí Matheo <lui.martins@minertecnologia.com>
2022-08-24 10:41:13 -0300

    adicionando typescript no projeto

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -4,7 +4,12 @@ import styles from './Comment.module.css';
 import { ThumbsUp, Trash } from 'phosphor-react';
 import { Avatar } from './Avatar';
 
-export function Comment({ content, onDeleteComment  }) {
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
   const [likeCount, setLikeCount] = useState(0);
 
   function handleDeleteComment() {
@@ -19,7 +24,11 @@ export function Comment({ content, onDeleteComment  }) {
 
   return (
     <div className={styles.comment}>
-      <Avatar hasBorder={false} src="https://github.com/diego3g.png" alt="" />
+      <Avatar
+        hasBorder={false}
+        src="https://github.com/diego3g.png"
+        alt=""
+      />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
