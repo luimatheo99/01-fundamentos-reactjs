de38745d8ae4f86bad93cfae77c21135778bb9b9
Luí Matheo <lui.martins@minertecnologia.com>
2022-08-24 09:30:15 -0300

    finalizando estrutura da aplicacao

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { Header } from "./components/Header";
+import { Post } from "./components/Post";
+import { Sidebar } from "./components/Sidebar";
 
 import styles from './App.module.css';
 
 import './global.css';
-import { Sidebar } from "./components/Sidebar";
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
       
       <div className={styles.wrapper}>
         <Sidebar />
-        <main>teeste</main>
+        <Post />
       </div>
     </div>
   )
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar';
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -10,7 +11,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/luimatheo99.png" />
+        <Avatar src="https://github.com/maykbrito.png" />
         <strong>Luí Matheo</strong>
         <span>Web Developer</span>
       </div>
